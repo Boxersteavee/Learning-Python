2082658ef930684bba41be67294608936c58d17f
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2021-11-14 13:45:40 +0000

    Coding For Beginners

---
+++ Coding For Beginners/Castle Game p21.py
@@ -0,0 +1,6 @@
+print("You are in a dark room in a mysterious castle.")
+print("In front of you are three doors. You must choose one.")
+playerChoice = input("Choose 1, 2 or 3...")
+if playerChoice == "1":
+    print("You find a room full of treasure")
+    print("GAME OVER! YOU WIN!")
---
+++ Coding For Beginners/do you like robot p17.py
@@ -0,0 +1,5 @@
+user_reply = input("Do you like robots? (Type Yes or No) ")
+if user_reply == "yes":
+    print("Beep Boop!")
+else:
+    print("Well, you should because i am a robot and you are surrounded by robots.")
